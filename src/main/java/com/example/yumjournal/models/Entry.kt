f8ff513dfe9aa44bb5dcca74950166ca0f363601
stephencarllicdan <stephencarl.licdan@hcdc.edu.ph>
2025-12-02 11:27:09 +0800

    Delete src directory

--- src/main/java/com/example/yumjournal/models/Entry.kt
+++
@@ -1,7 +0,0 @@
-package com.example.yumjournal.models
-
-data class Entry(
-    var id: String? = "",
-    var title: String? = "",
-    var description: String? = ""
-)
--- src/main/java/com/example/yumjournal/models/Food.kt
+++
@@ -1,8 +0,0 @@
-package com.example.yumjournal.models
-
-data class Food(
-    val name: String,
-    val category: String,
-    val ingredients: String,
-    val imageUrl: String
-)
